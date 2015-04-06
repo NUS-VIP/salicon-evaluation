1679624c6e056b2ae8d345a8476d05e8054ce7e2
Shane Huang <shannie.huang@gmail.com>
2015-04-06 20:48:33 +0800

    initial package layout

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    author='Shane Huang',
+    author_email = 'shannie.huang@gmail.com',
+    name='pycocoevalsal',
+    packages=['pycocoevalsal'],
+    package_dir = {'pycocoevalsal': 'pycocoevalsal'},
+    version='0.1'
+)
