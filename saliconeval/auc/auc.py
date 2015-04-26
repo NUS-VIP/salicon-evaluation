40ba121f9ac188e322b57105c13733ea8a780c53
Ming Jiang <jiangming@gmail.com>
2015-04-26 15:47:26 +0800

    add auc evaluation

--- saliconeval/auc/auc.py
+++ saliconeval/auc/auc.py
@@ -20,14 +20,57 @@ class AUC():
         self.imgs = self.saliconRes.imgs
 
 
-    def calc_score(self, gtsAnn, resAnn):
+    def calc_score(self, gtsAnn, resAnn, stepSize=.1, Nsplits=100):
         """
         Computer AUC score. A simple implementation
         :param gtsAnn : list of fixation annotataions
         :param resAnn : list only contains one element: the result annotation - predicted saliency map
         :return score: int : score
         """
-        return 0.0
+        
+        #result should be only one saliency map
+        image_id = resAnn[0]['image_id']
+        #get ground truth fixations and result saliency map
+        fixations = [ ann['fixations'] for ann in gtsAnn ] # fixations list
+        merged_fixations = [item for sublist in fixations for item in sublist]
+        salmap = self.saliconRes.decodeImage(resAnn[0]['saliency_map'])
+        
+        #get size of the original image
+        height,width = (self.imgs[image_id]['height'],self.imgs[image_id]['width'])
+        mapheight,mapwidth = np.shape(salmap)
+        salmap = scipy.ndimage.zoom(salmap, (float(height)/mapheight, float(width)/mapwidth), order=3)
+        salmap = (salmap - np.min(salmap))/(np.max(salmap) - np.min(salmap))
+        
+        S = salmap.reshape(-1)
+        F = self.saliconRes.buildFixMap(gtsAnn, False)
+        Sth = np.asarray([ salmap[y-1][x-1] for y,x in merged_fixations ])
+        
+        Nfixations = len(merged_fixations)
+        Npixels = len(S)
+        
+        # for each fixation, sample Nsplits values from anywhere on the sal map
+        r = np.random.randint(Npixels, size=(Nfixations,Nsplits))
+        
+        # sal map values at random locations
+        randfix = [S[x] for x in np.nditer(r, flags=['external_loop'], order='F')]
+        
+        # calculate AUC per random split (set of random locations)
+        auc = np.full(Nsplits, np.nan)
+        
+        for s in range(Nsplits):
+            curfix = randfix[s]
+            allthreshes = np.arange(0,np.max(np.concatenate((Sth, curfix), axis=0)),stepSize)
+            allthreshes = allthreshes[::-1]
+            tp = np.zeros(len(allthreshes)+2)
+            fp = np.zeros(len(allthreshes)+2)
+            tp[-1]=1.0
+            fp[-1]=1.0
+            tp[1:-1]=[float(np.sum(Sth >= thresh))/Nfixations for thresh in allthreshes]
+            fp[1:-1]=[float(np.sum(curfix >= thresh))/Nfixations for thresh in allthreshes]
+
+            auc[s] = np.trapz(tp,fp)
+
+        return np.mean(auc)
 
     def compute_score(self, gts, res):
         """
